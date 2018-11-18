55f4ab39ad0f22956b7fb77c3400f9fd88ff7822
kurt <kpdudek@bu.edu>
2018-11-18 11:31:03 -0500

    Added mouse configuration files

---
+++ Files/MouseSettings/mouse_acceleration_logitechwireless.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+xinput set-prop 'Logitech M510' 'Device Accel Profile' 0
+xinput set-prop 'Logitech M510' 'Device Accel Constant Deceleration' 2
+xinput set-prop 'Logitech M510' 'Device Accel Velocity Scaling' 80
+
+
+
+
+
+
+
---
+++ Files/MouseSettings/mouse_acceleration_pixart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#xinput set-prop 'PixArt Gaming Mouse' 'Device Accel Profile' 0
+#xinput set-prop 'PixArt Gaming Mouse' 'Device Accel Constant Deceleration' 2
+#xinput set-prop 'PixArt Gaming Mouse' 'Device Accel Velocity Scaling' 80
+
+xinput set-prop 9 'Device Accel Profile' 0
+xinput set-prop 9 'Device Accel Constant Deceleration' 2
+xinput set-prop 9 'Device Accel Velocity Scaling' 100
+
+
+
+
+
+
